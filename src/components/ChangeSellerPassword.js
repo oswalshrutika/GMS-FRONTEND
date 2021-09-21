cf133945f36f798cf29f794b6ba39a5a2c22c5e9
abhilash <abhilash.kamble376@gmail.com>
2021-09-21 23:19:19 +0530

    seller done

--- src/components/ChangeSellerPassword.js
+++ src/components/ChangeSellerPassword.js
@@ -1,45 +1,57 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import axios from 'axios'
-import {useHistory,useLocation } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
+import SellerMenuBar from './SellerMenuBar'
 const url = 'http://localhost:8080'
 
 
-const ChangeSellerPassword=()=>{
-    const[ password ,setnewPassword]=useState('')
-    const[ sellerId ,setSellerId]=useState('')
+const ChangeSellerPassword = () => {
+  const [password, setnewPassword] = useState('')
+  const [sellerId, setSellerId] = useState('')
 
-    const history = useHistory()
-   
-    const[seller,setSeller]=useState(JSON.parse(sessionStorage.getItem('seller')))
-   
-    
-    
-    // const location = useLocation()
-    // const user = location.state.user
-    const ChangePassword=(sellerId)=>{
-        axios.put(`http://localhost:8080/seller/update/${sellerId}`,{'password':password}).then((response)=>{
-          alert("success")
-          history.push('/home')
-        })
-}
-return (
-      <div className="container">
+  const history = useHistory()
+
+  const [seller, setSeller] = useState(JSON.parse(sessionStorage.getItem('seller')))
+
+
+
+  // const location = useLocation()
+  // const user = location.state.user
+  const ChangePassword = (sellerId) => {
+    axios.put(`http://localhost:8080/seller/update/${sellerId}`, { 'password': password }).then((response) => {
+      alert("success")
+      history.push('/home')
+    })
+  }
+  return (
+
+
+    <div className="grid grid-cols-4">
+      <div >
+        <SellerMenuBar />
+      </div>
+      <div className="col-span-3 p-3">
         <input
-                onChange={(e) => {
-                    setSellerId(e.target.value)
-                }}
-                className="form-control" type="hidden" />
-       
-         
-       
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter new Password.." onChange={(e)=>{
-            setnewPassword(e.target.value)
-            }}/>
-            <button type="submit" class="btn btn-primary" onClick={()=>{ChangePassword(seller.sellerId)}}>Change Password</button>
-         </div>
-         
-      
-);
+          onChange={(e) => {
+            setSellerId(e.target.value)
+          }}
+          className="form-control" type="hidden" />
+
+
+        <input type="password" className="form-control p-3 m-3" id="exampleInputPassword1" placeholder="Enter Old Password" onChange={(e) => {
+          setnewPassword(e.target.value)
+        }} />
+
+        <input type="password" className="form-control p-3 m-3" id="exampleInputPassword1" placeholder="Enter New Password" onChange={(e) => {
+          setnewPassword(e.target.value)
+        }} />
+        <button type="submit" className="btn btn-success m-3" onClick={() => { ChangePassword(seller.sellerId) }}>Change Password</button>
+      </div>
+    </div >
+
+
+
+  );
 }
 
 export default ChangeSellerPassword
