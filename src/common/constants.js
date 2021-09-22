6e07d3929087cb503e73532646218b3a6b05a2f8
abhilash <abhilash.kamble376@gmail.com>
2021-09-22 23:14:15 +0530

    added new url

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,6 @@
 // export const url ="http://localhost:8080"
  export const emailurl ="http://localhost:8081"
 
-export const url = "http://grocerybackend-env.eba-qqjcyppe.us-east-2.elasticbeanstalk.com"
+// export const url = "http://grocerybackend-env.eba-qqjcyppe.us-east-2.elasticbeanstalk.com"
+
+export const url = "http://ec2-18-119-1-194.us-east-2.compute.amazonaws.com:8080"
---
+++ src/components/Browse.js
@@ -0,0 +1,10 @@
+const Browse = () =>{
+    return(
+        <div>
+            <h2>this is browse page</h2>
+         
+        </div>
+    )
+}
+
+export default Browse
