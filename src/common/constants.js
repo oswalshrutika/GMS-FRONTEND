0026ce55aacabdc014227d02a4f5a9efba151342
D4_49388_Shrutika_oswal <oswal.shrutika.cdac@gmail.com>
2021-09-23 14:07:02 +0530

    Mail API deployed

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,5 +1,6 @@
 // export const url ="http://localhost:8080"
-  export const emailurl ="http://localhost:8081"
+  //export const emailurl ="http://localhost:8081"
+  export const emailurl="http://ec2-18-188-67-158.us-east-2.compute.amazonaws.com:5000"
 
 // export const url = "http://grocerybackend-env.eba-qqjcyppe.us-east-2.elasticbeanstalk.com"
 
