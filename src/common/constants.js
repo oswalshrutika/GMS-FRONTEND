d64bbad066f05f4484ab7884c66f373359273f02
D4_49388_Shrutika_oswal <oswal.shrutika.cdac@gmail.com>
2021-10-07 23:42:48 +0530

    update

--- src/common/constants.js
+++ src/common/constants.js
@@ -5,6 +5,6 @@
 
  export const emailurl="http://ec2-18-188-67-158.us-east-2.compute.amazonaws.com:5000"
 
-export const url = "http://grocerybackend-env.eba-qqjcyppe.us-east-2.elasticbeanstalk.com"
+//export const url = "http://grocerybackend-env.eba-qqjcyppe.us-east-2.elasticbeanstalk.com"
 
 export const url = "http://ec2-18-119-1-194.us-east-2.compute.amazonaws.com:8080"
